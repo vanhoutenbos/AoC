bd40c642f633a8f84f63c68f4919322f2284e61f
Jean-Paul van Houten <jean-paul@scorito.com>
2021-12-03 12:37:38 +0100

    Puzzle 1 completed

---
+++ AoC/AoC.Day1.Assignment1/Program.cs
@@ -0,0 +1,25 @@
+﻿string workingDirectory = Environment.CurrentDirectory;
+string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+string[] lines = File.ReadAllLines($"{projectDirectory}\\Input.txt");
+
+Console.WriteLine("Contents of WriteLines2.txt = ");
+int? previousNumber = null;
+var nrsHigherThanPrevious = 0;
+
+foreach (string line in lines)
+{
+    if (previousNumber == null)
+        previousNumber = int.Parse(line);
+
+    var currentNumber = int.Parse(line);
+    if(previousNumber < currentNumber)
+        nrsHigherThanPrevious++;   
+
+    previousNumber = currentNumber;
+}
+
+Console.WriteLine("high than previous nr = " + nrsHigherThanPrevious);
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
