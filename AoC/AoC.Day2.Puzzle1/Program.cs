267f2658e4bc3d6e55cc187dd763a295f8db7d70
Jean-Paul van Houten <jean-paul@scorito.com>
2021-12-03 13:20:01 +0100

    Puzzle 3 done

---
+++ AoC/AoC.Day2.Puzzle1/Program.cs
@@ -0,0 +1,29 @@
+﻿string workingDirectory = Environment.CurrentDirectory;
+string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+string[] lines = File.ReadAllLines($"{projectDirectory}\\Input.txt");
+var x = 0;
+var y = 0;
+
+foreach(var line in lines)
+{
+    var direction = line.Split(' ')[0];
+    var amount = int.Parse(line.Split(' ')[1]);
+
+    switch (direction)
+    {
+        case "forward":
+            x += amount;
+            break;
+        case "up":
+            y -= amount;
+            break;
+        case "down":
+            y += amount;
+            break;
+    }
+}
+
+Console.WriteLine(x * y);
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
